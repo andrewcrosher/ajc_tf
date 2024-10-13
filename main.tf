b48f4de718913505015ef53f1fc1d3766f2d153e
Andrew Crosher <andrewcrosher@outlook.com>
2024-10-13 11:43:08 +0100

    add adb dependencies

--- main.tf
+++ main.tf
@@ -61,10 +61,12 @@ resource "azurerm_databricks_workspace" "adb" {
 
 data "databricks_node_type" "smallest" {
   local_disk = true
+  depends_on = [azurerm_databricks_workspace.adb]
 }
 
 data "databricks_spark_version" "latest_lts" {
   long_term_support = true
+  depends_on        = [azurerm_databricks_workspace.adb]
 }
 
 resource "databricks_cluster" "single_node" {
@@ -72,7 +74,7 @@ resource "databricks_cluster" "single_node" {
   spark_version           = data.databricks_spark_version.latest_lts.id
   node_type_id            = data.databricks_node_type.smallest.id
   autotermination_minutes = 20
-
+  depends_on              = [azurerm_databricks_workspace.adb]
   spark_conf = {
     # Single-node
     "spark.databricks.cluster.profile" : "singleNode"
